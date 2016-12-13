6ee4ae030b13cbcc7eb29f86b32acc174a53bd38
Florian Amthor <f.amthor@gmx.de>
2016-12-13 21:54:50 +0100

    added local login and register functionality divided viewmodels into more folders

--- src/app.js
+++ src/app.js
@@ -1,3 +1,8 @@
+import {inject} from 'aurelia-framework';
+import TweetService from './services/tweet-service';
+
+@inject(TweetService)
+
 export class App {
 
   email = "florian@amthor.com";
@@ -5,14 +10,29 @@ export class App {
 
   loggedIn = false;
 
-
-  login(){
-    console.log(`Logging in ${this.email}`);
-    this.loggedIn = true;
+  constructor(ts){
+    this.tweetService = ts;
   }
+
+  login(e) {
+    console.log(`Trying to log in ${this.email}`);
+    const status = this.tweetService.login(this.email, this.password);
+    this.prompt = status.message;
+    this.loggedIn = status.success;
+  }
+
 
   logout(){
     console.log(`Logging out ${this.email}`);
     this.loggedIn = false;
+  }
+
+  register(e) {
+    this.tweetService.register(this.firstName, this.lastName, this.registerEmail, this.registerPassword);
+    this.firstName = null;
+    this.lastName = null;
+    this.registerEmail = null;
+    this.registerPassword = null;
+
   }
 }
--- src/services/tweet-service.js
+++ src/services/tweet-service.js
@@ -14,6 +14,43 @@ export default class TweetService {
     this.comments = data.comments;
   }
 
+  login(email, password) {
+    const status = {
+      success: false,
+      message: ''
+    };
+
+    console.log(this.users[email]);
+    let user = {};
+    for(let i = 0; i < this.users.length; i++){
+      if (this.users[i].email === email){
+        user = this.users[i];
+      }
+    }
+    if (user != null) {
+      if (user.password === password) {
+        status.success = true;
+      } else {
+        status.message = 'Incorrect password';
+      }
+    } else {
+      status.message = 'Unknown user';
+    }
+
+    return status;
+  }
+
+  register(firstName, lastName, email, password) {
+    const newUser = {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: password,
+      isAdmin: false
+    };
+    this.users.push(newUser);
+  }
+
   tweet(author, date, content, image) {
     let tweet = {
       author: author,
